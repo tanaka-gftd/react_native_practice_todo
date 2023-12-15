e369ebc783f81ff2d50e914bde5e45902662eb93
田中冬紀 <winter.tanaka@gmail.com>
2023-12-15 14:44:22 +0900

    参考サイト通り、ToDoアプリのベース作成終了

--- App.js
+++ App.js
@@ -1,20 +1,173 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//ReactのHookを導入
+import React, {useState} from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+//ReactNativeのコンポーネントを導入
+import { 
+    StyleSheet, 
+    Text, 
+    View, 
+    TextInput, 
+    TouchableOpacity, 
+    FlatList 
+} from 'react-native';
+
+const App = () => {
+
+    //フォームに書き込まれた内容を保持
+    const [task, setTask] = useState("");
+
+    //登録されたタスクの保持用
+    const [taskArray, setTaskArray] = useState([]);
+
+    //タスクのエディット用
+    const [editIndex, setEditIndex] = useState(-1);
+
+    //タスクの新規登録orタスク名の変更を保存
+    const handleAddTask = () => {
+
+        //フォームにタスク名が入力されていれば、以下の処理を行う
+        if(task){
+            
+            if(editIndex !== -1){
+                //「editIndex !== -1」の場合、つまり、タスク名を変更中の場合なら、変更されたタスク名を保存し終了
+                const updatedTasks = [...taskArray];
+                updatedTasks[editIndex] = task;
+                setTaskArray(updatedTasks);
+                setEditIndex(-1);
+            }else{
+                //新規登録の場合は配列に追加
+                setTaskArray([...taskArray, task]);
+            }
+
+            //処理後、フォーム欄を空欄にする
+            setTask("");
+        }
+    };
+
+    //タスク名の変更
+    const handleEditTask = (index) => { 
+        const taskToEdit = taskArray[index]; 
+        setTask(taskToEdit); //変更したいタスクの名前をフォームに表示させる
+        setEditIndex(index); 
+    }; 
+
+    //タスクの削除
+    //taskArrayから指定されたインデックスの要素(タスク)を削除して、 setTaskArrayでセットし直す
+    const handleDeleteTask = (index) => { 
+        const updatedTasks = [...taskArray]; 
+        updatedTasks.splice(index, 1); 
+        setTaskArray(updatedTasks); 
+    }; 
+
+    //現在登録されているタスクを表示していく
+    //タスク名変更とタスク削除も追加
+    const renderItem = ({item, index}) => (
+        <View style={styles.task}>
+            <Text style={styles.itemList}>{item}</Text>
+            <View style={styles.taskButtons}>
+
+                <TouchableOpacity 
+                    onPress={()=>handleEditTask(index)}
+                >
+                    <Text style={styles.editButton}>タスク名変更</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity 
+                    onPress={() => handleDeleteTask(index)}
+                >
+                    <Text style={styles.deleteButton}>タスク削除</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>シンプルなToDoアプリ</Text>
+            <TextInput
+                style={styles.input}
+                placeholder='タスク名を入力してください'
+                value={task}
+                onChangeText={(text) => setTask(text)}
+            />
+            <TouchableOpacity 
+                style={styles.addButton} 
+                onPress={handleAddTask}
+            >
+                <Text style={styles.addButtonText}>
+                  {editIndex !== -1 ? "変更を保存" : "タスクを追加"}
+                </Text>
+            </TouchableOpacity>
+            <FlatList
+                data={taskArray} 
+                renderItem={renderItem} 
+                keyExtractor={(item, index) => index.toString()} 
+            />
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: { 
+        flex: 1, 
+        padding: 40, 
+        marginTop: 40, 
+    }, 
+    title: { 
+        fontSize: 24, 
+        fontWeight: "bold", 
+        marginBottom: 20, 
+    }, 
+    heading: { 
+        fontSize: 30, 
+        fontWeight: "bold", 
+        marginBottom: 7, 
+        color: "green", 
+    }, 
+    input: { 
+        borderWidth: 3, 
+        borderColor: "#ccc", 
+        padding: 10, 
+        marginBottom: 10, 
+        borderRadius: 10, 
+        fontSize: 18, 
+    }, 
+    addButton: { 
+      backgroundColor: "green", 
+      padding: 10, 
+      borderRadius: 5, 
+      marginBottom: 10, 
+    }, 
+    addButtonText: { 
+      color: "white", 
+      fontWeight: "bold", 
+      textAlign: "center", 
+      fontSize: 18, 
+    }, 
+    task: { 
+      flexDirection: "row", 
+      justifyContent: "space-between", 
+      alignItems: "center", 
+      marginBottom: 15, 
+      fontSize: 18, 
+    }, 
+    itemList: { 
+      fontSize: 19, 
+    }, 
+    taskButtons: { 
+      flexDirection: "row", 
+    }, 
+    editButton: { 
+      marginRight: 10, 
+      color: "green", 
+      fontWeight: "bold", 
+      fontSize: 18, 
+    }, 
+    deleteButton: { 
+      color: "red", 
+      fontWeight: "bold", 
+      fontSize: 18, 
+    }, 
 });
+
+export default App;

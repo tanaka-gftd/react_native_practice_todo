214a0ec98ea4e8631fd1592549e07bbadf78cbc3
田中冬紀 <winter.tanaka@gmail.com>
2023-12-19 10:22:16 +0900

    変数名を変更、コードの整形

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ import {
 const App = () => {
 
     //フォームに書き込まれた内容を保持
-    const [task, setTask] = useState("");
+    const [taskName, setTaskName] = useState("");
 
     //登録されたタスクの保持用
     const [taskArray, setTaskArray] = useState([]);
@@ -26,28 +26,28 @@ const App = () => {
     const handleAddTask = () => {
 
         //フォームにタスク名が入力されていれば、以下の処理を行う
-        if(task){
+        if(taskName){
             
             if(editIndex !== -1){
                 //「editIndex !== -1」の場合、つまり、タスク名を変更中の場合なら、変更されたタスク名を保存し終了
                 const updatedTasks = [...taskArray];
-                updatedTasks[editIndex] = task;
+                updatedTasks[editIndex] = taskName;
                 setTaskArray(updatedTasks);
                 setEditIndex(-1);
             }else{
                 //新規登録の場合は配列に追加
-                setTaskArray([...taskArray, task]);
+                setTaskArray([...taskArray, taskName]);
             }
 
             //処理後、フォーム欄を空欄にする
-            setTask("");
+            setTaskName("");
         }
     };
 
     //タスク名の変更
     const handleEditTask = (index) => { 
         const taskToEdit = taskArray[index]; 
-        setTask(taskToEdit); //変更したいタスクの名前をフォームに表示させる
+        setTaskName(taskToEdit); //変更したいタスクの名前をフォームに表示させる
         setEditIndex(index); 
     }; 
 
@@ -63,18 +63,17 @@ const App = () => {
     //タスク名変更とタスク削除も追加
     const renderItem = ({item, index}) => (
         <View style={styles.task}>
-            <Text style={styles.itemList}>{item}</Text>
+            <TouchableOpacity onPress={()=>showDetailWindow(index)}>
+                <Text style={styles.itemList}>{item}</Text>
+            </TouchableOpacity>
+            
             <View style={styles.taskButtons}>
 
-                <TouchableOpacity 
-                    onPress={()=>handleEditTask(index)}
-                >
+                <TouchableOpacity onPress={()=>handleEditTask(index)}>
                     <Text style={styles.editButton}>タスク名変更</Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity 
-                    onPress={() => handleDeleteTask(index)}
-                >
+                <TouchableOpacity onPress={() => handleDeleteTask(index)}>
                     <Text style={styles.deleteButton}>タスク削除</Text>
                 </TouchableOpacity>
             </View>
@@ -87,15 +86,15 @@ const App = () => {
             <TextInput
                 style={styles.input}
                 placeholder='タスク名を入力してください'
-                value={task}
-                onChangeText={(text) => setTask(text)}
+                value={taskName}
+                onChangeText={(text) => setTaskName(text)}
             />
             <TouchableOpacity 
                 style={styles.addButton} 
                 onPress={handleAddTask}
             >
                 <Text style={styles.addButtonText}>
-                  {editIndex !== -1 ? "変更を保存" : "タスクを追加"}
+                    {editIndex !== -1 ? "変更を保存" : "タスクを追加"}
                 </Text>
             </TouchableOpacity>
             <FlatList
@@ -133,40 +132,40 @@ const styles = StyleSheet.create({
         fontSize: 18, 
     }, 
     addButton: { 
-      backgroundColor: "green", 
-      padding: 10, 
-      borderRadius: 5, 
-      marginBottom: 10, 
+        backgroundColor: "green", 
+        padding: 10, 
+        borderRadius: 5, 
+        marginBottom: 10, 
     }, 
     addButtonText: { 
-      color: "white", 
-      fontWeight: "bold", 
-      textAlign: "center", 
-      fontSize: 18, 
+        color: "white", 
+        fontWeight: "bold", 
+        textAlign: "center", 
+        fontSize: 18, 
     }, 
     task: { 
-      flexDirection: "row", 
-      justifyContent: "space-between", 
-      alignItems: "center", 
-      marginBottom: 15, 
-      fontSize: 18, 
+        flexDirection: "row", 
+        justifyContent: "space-between", 
+        alignItems: "center", 
+        marginBottom: 15, 
+        fontSize: 18, 
     }, 
     itemList: { 
-      fontSize: 19, 
+        fontSize: 19, 
     }, 
     taskButtons: { 
-      flexDirection: "row", 
+        flexDirection: "row", 
     }, 
     editButton: { 
-      marginRight: 10, 
-      color: "green", 
-      fontWeight: "bold", 
-      fontSize: 18, 
+        marginRight: 10, 
+        color: "green", 
+        fontWeight: "bold", 
+        fontSize: 18, 
     }, 
     deleteButton: { 
-      color: "red", 
-      fontWeight: "bold", 
-      fontSize: 18, 
+        color: "red", 
+        fontWeight: "bold", 
+        fontSize: 18, 
     }, 
 });
 
